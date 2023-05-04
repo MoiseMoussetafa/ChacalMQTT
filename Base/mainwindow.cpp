a6b49c9599ac6cada75c2039825f205aed87c6f1
Moïse Moussetafa <72506988+MoiseMoussetafa@users.noreply.github.com>
2023-05-04 14:08:17 +0200

    Update mainwindow.cpp
    Complete to sync with .ui

--- Base/mainwindow.cpp
+++ Base/mainwindow.cpp
@@ -1,15 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QtMqtt/QMqttClient>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Affichage des informations dans les textEdit
+    ui->textEdit1->setPlainText("Broker : mqtt://broker.emqx.io:1883");
+    ui->textEdit2->setPlainText("Topic : /ynov/bordeaux/NomDuGitHubDuGroupe");
+    ui->textEdit3->setPlainText("QoS 2");
+    ui->textEdit4->setPlainText("Message décodé = ");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-

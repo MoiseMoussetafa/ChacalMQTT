0bb28b13a53077bb969194dc2a755b40ee05d5e9
Matteo-Penaud <matteo.penaud@ynov.com>
2023-05-05 11:07:28 +0200

    Image loading and encoding working

--- Drone/drone.pro
+++ Drone/drone.pro
@@ -1,5 +1,3 @@
-QT -= gui
-
 CONFIG += c++17 console
 CONFIG -= app_bundle
 
@@ -7,8 +5,7 @@ CONFIG -= app_bundle
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SUBDIRS += \
-    src/
+INCLUDEPATH += \
     inc/
 
 SOURCES += \
@@ -18,3 +15,9 @@ SOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    inc/common.h
+
+RESOURCES += \
+    ressources.qrc
--- Drone/src/main.cpp
+++ Drone/src/main.cpp
@@ -1,8 +1,71 @@
+#include <iostream>
 #include <QCoreApplication>
+#include <QImage>
+#include "common.h"
+#include <QDebug>
+#include <QFile>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int32_t main(int32_t s32_argc, char_t *c_argv[])
 {
-    QCoreApplication a(argc, argv);
+    QCoreApplication a(s32_argc, c_argv);
+
+    QImage image(":/images/img/DroneIMG37337.png");
+
+    if(image.isNull())
+    {
+        cout << "ERROR : Can't load image" << endl;
+        return -1;
+    }
+
+    QString gpsCoordinates = "37°42'41.9\"S 144°59'33.0\"E";
+    QString gpsBinary;
+    for (int32_t i = 0; i < gpsCoordinates.size(); ++i)
+    {
+        QChar c = gpsCoordinates[i];
+
+        gpsBinary.append(QString("%1").arg(c.unicode(), 8, 2, QChar('0')));
+    }
+
+    qDebug() << gpsBinary << Qt::endl;
+
+    for (int32_t i = 0; i < gpsBinary.size(); ++i)
+    {
+        int32_t x = i % image.width();
+        int32_t y = i / image.width();
+        qDebug() << "x: " << x << "| y : " << y << Qt::endl;
+
+        QRgb pixel = image.pixel(x, y);
+
+        int32_t alpha = qAlpha(pixel);
+        int32_t red = qRed(pixel);
+        int32_t green = qGreen(pixel);
+        int32_t blue = qBlue(pixel);
+
+        if (alpha == 0)
+        {
+            alpha = 1;
+        }
+
+        if (i < image.width() * image.height())
+        {
+            int32_t bit = gpsBinary[i].digitValue();
+            red = (red & ~1) | bit;
+        }
+
+        qDebug() << pixel << Qt::endl;
+
+        image.setPixel(x, y, qRgba(red, green, blue, alpha));
+    }
+
+    QFile outputFile("DroneIMG37337_with_gps.png");
+    if (!outputFile.open(QIODevice::WriteOnly))
+    {
+        qDebug() << "Erreur : Impossible d'enregistrer l'image";
+        return -1;
+    }
+    image.save(&outputFile, "PNG");
 
     return a.exec();
 }
